bb34c94ad204ad2e18531b5e5d3d218895dcefb3
cqxcool88 <136910901@qq.com>
2018-01-26 13:16:12 +0800

    arrangement

---
+++ c++/sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "enter two number:" << std::endl;
+	int v1,v2;
+	std::cin >> v1 >>v2;
+	std::cout << "The sum of " << v1 << " and " << v2
+	<< " is " << v1+v2 << std::endl;
+	return 0;
+}
