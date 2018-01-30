02730e1cef9d601a6db8361f14c4d29ff57c06ef
root <root@DESKTOP-4F78TLJ.localdomain>
2018-01-30 20:03:17 +0800

    amend construct.cpp

--- c++/construct.cpp
+++ c++/construct.cpp
@@ -4,21 +4,31 @@ using namespace std;
 class Box
 {
     public:
-        Box();
+		/* 2 constuct function */
+        //Box();	//exti define 
+		Box():height(2),width(2),length(2){}
         Box(int h, int w, int len):height(h),width(w),length(len){}
 
         int volume();
+		int change_value();
     private:
         int height;
         int width;
         int length;
 };
 
+/* 
 Box::Box()
 {
     height = 10;
     width = 10;
     length =10;
+}
+*/
+
+int Box::change_value()
+{
+	height = 0;	
 }
 
 int Box::volume()
@@ -29,6 +39,8 @@ int Box::volume()
 int main()
 {
     Box box1;
+    cout << box1.volume() << endl;
+	box1.change_value();
     cout << box1.volume() << endl;
     Box box2(15,30,25);
     cout << box2.volume() << endl;
