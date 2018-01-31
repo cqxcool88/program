797ac60f4bb07d4bdaa4211555c2bc639d9d1051
root <root@DESKTOP-4F78TLJ.localdomain>
2018-01-31 09:45:24 +0800

    add map

---
+++ c++/map.cpp
@@ -0,0 +1,22 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int i;
+	map<int, string> mapStudent;
+	mapStudent.insert(pair<int, string>(1, "student_one"));
+	mapStudent.insert(pair<int, string>(2, "student_two"));
+	mapStudent.insert(pair<int, string>(3, "student_three"));
+//	mapStudent.insert(1, "student_one");
+//	mapStudent.insert(2, "student_two");
+//	mapStudent.insert(3, "student_three");
+	i = mapStudent.count(4);
+	cout << "cout:" << i << endl;
+	map<int, string>::iterator  iter;
+	for(iter = mapStudent.begin(); iter != mapStudent.end(); iter++) {
+		cout << iter->first << "   " << iter->second << endl;
+	}
+}
